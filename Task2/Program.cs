6149a46f96de848ac8be7faf25ac1fc8b5cb3224
AlbulOlya <albul.olya@gmail.com>
2023-02-03 22:41:12 +0500

    Task 2 solved

---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int numb = int.Parse(Console.ReadLine() ??"0");
+int remainder = 0;
+while (numb >0)
+{
+    remainder=remainder+numb%10;
+    numb = numb/10;
+}
+Console.WriteLine($"Сумма цифр числа равна {remainder}");
