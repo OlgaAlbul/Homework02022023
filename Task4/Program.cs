9b8f7e199e582d3cb8f0d7e09f6f4020a1615244
AlbulOlya <albul.olya@gmail.com>
2023-02-03 22:55:32 +0500

    Task 4 solved

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int numb = int.Parse(Console.ReadLine() ??"0");
+int cube;
+int count = 1;
+while (count <= numb)
+{
+    cube = count*count*count;
+    if (cube%2==0)
+    {
+        Console.WriteLine($"Число {count} \t куб {cube}");
+        count++;
+    }
+    else
+    count++;
+}
