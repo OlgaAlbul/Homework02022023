4b5d69657856c6aaf32fc8cc8077aed08c4bbe80
AlbulOlya <albul.olya@gmail.com>
2023-02-03 22:47:04 +0500

    Task 3 solved

---
+++ Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int numb = int.Parse(Console.ReadLine() ??"0");
+int res = 1;
+for (int count = 1; count<=numb; count++)
+{
+    res = res*count;
+}
+Console.WriteLine($"Произведение чисел от 1 до {numb} равно {res}");
